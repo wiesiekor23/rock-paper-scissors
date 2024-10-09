ed54c4391f709ab7bef23705504c1316d2df29f1
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2024-10-10 00:46:50 +0100

    Write computer choice logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,12 @@
-console.log("info");
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 100);
+    if (randomNumber % 3 === 0) {
+        return "rock";
+    } else if (randomNumber % 3 == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
